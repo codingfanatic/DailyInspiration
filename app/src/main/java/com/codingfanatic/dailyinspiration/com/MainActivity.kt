83e301ad1e1efe899234cae9cd52734afd76e753
codingfanatic <codingfanatic@github.com>
2021-08-22 06:20:49 -0400

    Integrated Firebase and added test code
    Added a function basicReadWrite() to MainActivity.kt
    Tested different methods for reading and writing data from the database.

--- app/src/main/java/com/codingfanatic/dailyinspiration/com/MainActivity.kt
+++ app/src/main/java/com/codingfanatic/dailyinspiration/com/MainActivity.kt
@@ -2,10 +2,57 @@ package com.codingfanatic.dailyinspiration.com
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "KotlinActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        basicReadWrite()
+    }
+    fun basicReadWrite() {
+        // Use this string as the message column reference
+        // in the Realtime Database
+        val messageColumnInFirebase = "message3goaskeeeew"
+
+        //START WRITING MESSAGE TO DB
+        //Writing to your database.
+        val database = Firebase.database //Create a Firebase object variable
+        val myRef = database.getReference(messageColumnInFirebase)
+
+        // Read from the database
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue<String>()
+                Log.d(TAG, "Value is: $value")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
+
+        myRef.removeValue()
+        myRef.setValue("KILL ME", 3)
+        myRef.setValue("KILL ME...sike nah jk", 1)
+        //END WRITING TO DB
+
+
+
     }
 }
