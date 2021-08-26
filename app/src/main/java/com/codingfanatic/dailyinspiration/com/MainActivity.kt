551eb2d0131b84e20bb6ec606e9887858448e5c7
codingfanatic <codingfanatic@github.com>
2021-08-26 15:14:21 -0400

    App generates a random quote onResume Overrode the onResume method to make the app execute readQuote() Now the app will display a random quote each time it is opened or resumed

--- app/src/main/java/com/codingfanatic/dailyinspiration/com/MainActivity.kt
+++ app/src/main/java/com/codingfanatic/dailyinspiration/com/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import java.time.DayOfWeek
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -21,38 +22,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       // writeQuote()
-        //In main, set the value of the TextView to the content of the variable
-        val quoteTextView = findViewById<TextView>(R.id.quoteTextView) as TextView
-        quoteTextView.setText(readQuote())
+         readQuote()
+         //writeQuote()
     }
 
-    //Return a String. Namely, the value in firebase
-    fun readQuote(): String {
+    override fun onResume() {
+        super.onResume()
+        setContentView(R.layout.activity_main)
+        readQuote()
+    }
 
-        val messageColumnInFirebase = "quoteToBeDisplayed"
+    //Read a String from the Realtime Database and use it as the TextView
+    fun readQuote() {
+        val randomQuote = (0..6).random()
+        val messageColumnInFirebase = "quoteToBeDisplayed" + "$randomQuote"
         val database = Firebase.database //Create a Firebase object variable
         val myRef = database.getReference(messageColumnInFirebase)
-        //ARGH KEEPS RETURNING THIS VALUE INSTEAD OF REALTIME DATABASE VALUE
-        val randomStringHere = ""
+        val quoteTextView = findViewById<TextView>(R.id.quoteTextView) as TextView
 
-        // Read from the database
+        // Read from the database via listener
         myRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
-                val randomStringHere = dataSnapshot.getValue<String>()
-                Log.d(TAG, "Value is: $randomStringHere")
+                val value = dataSnapshot.getValue<String>()
+                quoteTextView.setText(value)
+                Log.d(TAG, "Value is: $value")
             }
             override fun onCancelled(error: DatabaseError) {
                 // Failed to read value
                 Log.w(TAG, "Failed to read value.", error.toException())
             }
         })
-        Log.d(TAG, "Value is: $randomStringHere")
-        return randomStringHere
     }
-
+/*
+    //Write a new quote to the Realtime Datatbase
     fun writeQuote() {
         val messageColumnInFirebase = "quoteToBeDisplayed"
         //START WRITING MESSAGE TO DB
@@ -61,4 +65,5 @@ class MainActivity : AppCompatActivity() {
         val myRef = database.getReference(messageColumnInFirebase)
         myRef.setValue("You are the master of your judgements, your decisions, and your actions.")
     }
+            */
 }
